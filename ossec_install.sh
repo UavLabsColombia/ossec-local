3840672fe8004faeb74626866c5e94b661f81af2
Dorance Martinez Cortes <dorancemc@gmail.com>
2016-09-10 21:58:32 -0500

    Create ossec_install.sh

---
+++ ossec_install.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#
+# @dorancemc - 10-sep-2016
+#
+# Script para installar ossec
+# Validado en : Debian 7+, Ubuntu 16+, Centos 6+
+#
+#
+
+linux_variant() {
+  if [ -f "/etc/debian_version" ]; then
+    distro="debian"
+  elif [ -f "/etc/redhat-release" ]; then
+    distro="redhat"
+  else
+    distro="unknown"
+  fi
+}
+
+command_exists () {
+    type "$1" &> /dev/null ;
+}
+
+debian() {
+  if ! command_exists lsb_release ; then
+    apt-get install -y lsb-release
+  fi
+  distro=$(lsb_release -s -i | tr '[:upper:]' '[:lower:]')
+  version=$(lsb_release -s -c )
+  echo "deb http://ossec.wazuh.com/repos/apt/${distro} ${version} main" > /etc/apt/sources.list.d/ossec.list
+  apt-key adv --fetch-keys http://ossec.wazuh.com/repos/apt/conf/ossec-key.gpg.key
+  apt-get update
+  apt-cache search ossec
+  apt-get install ossec-hids -y
+}
+
+redhat() {
+  wget -q https://www.atomicorp.com/RPM-GPG-KEY.art.txt 1>/dev/null 2>&1
+  wget -q https://www.atomicorp.com/RPM-GPG-KEY.atomicorp.txt 1>/dev/null 2>&1
+  rpm -import RPM-GPG-KEY.art.txt >/dev/null 2>&1
+  rpm -import RPM-GPG-KEY.atomicorp.txt >/dev/null 2>&1
+  wget -q http://updates.atomicorp.com/channels/atomic/centos/6/x86_64/RPMS/atomic-release-1.0-21.el6.art.noarch.rpm
+  rpm -Uvh atomic-release-1.0-21.el6.art.noarch.rpm
+  yum install -y ossec-hids-server
+}
+
+unknown() {
+  echo "distro no soportada por este script :( "
+}
+
+run_core() {
+  linux_variant
+  $distro
+}
+
+run_core

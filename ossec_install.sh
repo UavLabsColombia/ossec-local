913fac4e14684a7175acff29a0ef3ece173d9d61
Dorance Martinez Cortes <dorancemc@gmail.com>
2016-10-05 10:33:51 -0500

    Update ossec_install.sh

--- ossec_install.sh
+++ ossec_install.sh
@@ -3,7 +3,7 @@
 # @dorancemc - 10-sep-2016
 #
 # Script para installar ossec
-# Validado en : Debian 7+, Ubuntu 16+, Centos 6+
+# Validado en : Debian 7+, Ubuntu 16+, Centos 6+, SuSE 13+
 #
 #
 
@@ -12,6 +12,8 @@ linux_variant() {
     distro="debian"
   elif [ -f "/etc/redhat-release" ]; then
     distro="redhat"
+  elif [ -f "/etc/SuSE-release" ]; then
+    distro="suse"
   else
     distro="unknown"
   fi
@@ -27,10 +29,10 @@ debian() {
   fi
   distro=$(lsb_release -s -i | tr '[:upper:]' '[:lower:]')
   version=$(lsb_release -s -c )
-  echo "deb http://ossec.wazuh.com/repos/apt/${distro} ${version} main" > /etc/apt/sources.list.d/ossec.list
-  apt-key adv --fetch-keys http://ossec.wazuh.com/repos/apt/conf/ossec-key.gpg.key
-  apt-get update
-  apt-cache search ossec
+  echo "deb http://ossec.wazuh.com/repos/apt/${distro} ${version} main" > /etc/apt/sources.list.d/ossec.list &&
+  apt-key adv --fetch-keys http://ossec.wazuh.com/repos/apt/conf/ossec-key.gpg.key &&
+  apt-get update &&
+  apt-cache search ossec &&
   if [ "$install" = "server" ]; then
     apt-get install ossec-hids -y
   else
@@ -42,17 +44,36 @@ redhat() {
   if ! command_exists wget ; then
     yum install wget -y
   fi
-  wget -q https://www.atomicorp.com/RPM-GPG-KEY.art.txt 1>/dev/null 2>&1
-  wget -q https://www.atomicorp.com/RPM-GPG-KEY.atomicorp.txt 1>/dev/null 2>&1
-  rpm -import RPM-GPG-KEY.art.txt >/dev/null 2>&1
-  rpm -import RPM-GPG-KEY.atomicorp.txt >/dev/null 2>&1
-  wget -q http://updates.atomicorp.com/channels/atomic/centos/6/x86_64/RPMS/atomic-release-1.0-21.el6.art.noarch.rpm
-  rpm -Uvh atomic-release-1.0-21.el6.art.noarch.rpm
+  wget -q https://www.atomicorp.com/RPM-GPG-KEY.art.txt 1>/dev/null 2>&1 &&
+  wget -q https://www.atomicorp.com/RPM-GPG-KEY.atomicorp.txt 1>/dev/null 2>&1 &&
+  rpm -import RPM-GPG-KEY.art.txt >/dev/null 2>&1 &&
+  rpm -import RPM-GPG-KEY.atomicorp.txt >/dev/null 2>&1 &&
+  wget -q http://updates.atomicorp.com/channels/atomic/centos/6/x86_64/RPMS/atomic-release-1.0-21.el6.art.noarch.rpm &&
+  rpm -Uvh atomic-release-1.0-21.el6.art.noarch.rpm &&
   if [ "$install" == "server" ]; then
     yum install -y ossec-hids-server
   else
     yum install -y ossec-hids-client
   fi  
+}
+
+suse (){
+  if ! command_exists wget ; then
+    zypper --non-interactive install wget 
+  fi
+  zypper --non-interactive install gcc &&
+  mkdir -p /tmp/ossec && cd /tmp/ossec &&
+  wget https://github.com/ossec/ossec-hids/archive/v2.8.3.tar.gz &&
+  tar -zxvf v2.8.3 &&
+  cd ossec-hids-2.8.3/src/ &&
+  if [ "$install" == "server" ]; then
+    make setlocal && make all && make local
+  else
+    make setagent && make all && make agent
+  fi 
+  cp init/ossec-hids-suse.init /etc/init.d/ossec-hids
+  chmod 755 /etc/init.d/ossec-hids
+  chkconfig --add ossec-hids
 }
 
 unknown() {
